8b49f02aa181c556345697c9464d889143ed9410
Sahal-Ahmad7 <sahalahmad051@gmail.com>
2025-08-14 10:30:52 -0700

    i willl learn about VARIABLES in JS

---
+++ 02_Variables.js
@@ -0,0 +1,13 @@
+const Account_ID = "39"
+let password = "****"
+var Account_gmail = "Sazimc@google.com"
+Account_Active_Duration = "14 hours"
+
+// Account_ID = 3
+password = "111"
+Account_gmail = "Sa@google.com"
+Account_Active_Duration = "10  hours"
+
+console.log(Account_ID);
+
+console.table([Account_gmail , Account_ID , Account_Active_Duration , password])
