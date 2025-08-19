e9fd9e67d00e1022e47e8b266d6be4c6f73e34d9
Sahal-Ahmad7 <sahalahmad051@gmail.com>
2025-08-18 20:51:18 -0700

    Arrow Functions IS Here

---
+++ 11_Arrow_Function.js
@@ -0,0 +1,12 @@
+let Admissionform = {
+    name: "Sahal",
+    class:8,
+    fee : 11299,
+
+    Welcome_msg:function(){
+        console.log(`${this.name}, Welcome to your new school.`);
+    };
+    
+}
+
+Welcome_msg()
