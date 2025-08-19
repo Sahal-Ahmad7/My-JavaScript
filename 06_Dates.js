028e017a0d6f8e2ab0fd1dfbd686f6eb1bed8ccf
Sahal-Ahmad7 <sahalahmad051@gmail.com>
2025-08-18 20:46:49 -0700

    Dates in JS

---
+++ 06_Dates.js
@@ -0,0 +1,15 @@
+let mydata = new Date()
+//console.log(mydata.toString());  
+
+//console.log(mydata.toDateString());
+
+//console.log(mydata.toISOString())
+
+//console.log(mydata.toLocaleDateString());
+
+let NEW_DATE = new Date()
+console.log(NEW_DATE.toString());
+console.log(NEW_DATE.getFullYear());
+
+
+
